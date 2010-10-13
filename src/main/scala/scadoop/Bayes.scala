b95565dc11eeb2f06c27573e3d007450fe0731f0
Miles Egan <milesegan@gmail.com>
2010-10-13 12:06:52 +0700

    clean up bayes training flow

--- src/main/scala/scadoop/Bayes.scala
+++ src/main/scala/scadoop/Bayes.scala
@@ -3,37 +3,15 @@
 package scadoop
 
 import cascading.flow.{Flow, FlowConnector, FlowProcess}
-import cascading.operation.{Aggregator, AggregatorCall, BaseOperation, Debug, Function, FunctionCall, Identity}
+import cascading.operation.{Aggregator, AggregatorCall, BaseOperation, Debug, Function, FunctionCall, Identity, Insert}
 import cascading.operation.aggregator.Count
 import cascading.operation.regex.RegexSplitter
 import cascading.pipe.{Each, Every, GroupBy, Pipe}
 import cascading.scheme.{Scheme, TextDelimited, TextLine}
-import cascading.tap.{Hfs, Tap}
-import cascading.tap.SinkMode
+import cascading.tap.{Hfs, MultiSourceTap, Tap, SinkMode}
 import cascading.tuple.{Fields, Tuple}
-import collection.mutable.{Map => MMap}
 
-class BayesSplitter 
-extends BaseOperation[Array[String]](new Fields("dummy", "class", "features")) 
-with Function[Array[String]] {
-
-  def operate(fp: FlowProcess, fc: FunctionCall[Array[String]]) = {
-    val tokens = fc.getArguments.getString("tokens")
-    val str = tokens.toString.split(",")
-    var names = fc.getContext
-    if (names == null) {
-      fc.setContext(str.tail)
-    }
-    else {
-      val feats = new Tuple()
-      for (f <- names zip str.tail) feats.add(f._1 + "++" + f._2)
-      fc.getOutputCollector().add(new Tuple("-", str.head, feats))
-    }
-  }
-
-}
-
-class BayesAggregator
+class BayesAggregator(featureNames: Seq[String])
 extends BaseOperation[BayesClassifier](2, new Fields("bayes")) 
 with Aggregator[BayesClassifier] {
 
@@ -43,9 +21,9 @@ with Aggregator[BayesClassifier] {
 
   def aggregate(fp: FlowProcess, ac: AggregatorCall[BayesClassifier]) {
     val bc = ac.getContext
-    val klass = ac.getArguments.getString("class")
-    val featT = ac.getArguments.getObject("features").asInstanceOf[Tuple]
-    val feats = (0 until featT.size).map(featT.getString(_))
+    val tokens = ac.getArguments.getString("line").split(",").toList
+    val klass = tokens.head
+    val feats = (featureNames zip tokens.tail).map{case(n,v) => n + "++" + v}
     val newbc = bc.addSample(feats, klass)
     ac.setContext(newbc)
   }
@@ -59,25 +37,55 @@ with Aggregator[BayesClassifier] {
 
 object Bayes {
 
+  def readFile(path: String): Seq[String] = {
+    import org.apache.hadoop.mapred.JobConf
+    val tap = new Hfs(new TextLine(), path)
+    val tuples = tap.openForRead(new JobConf())
+    val b = collection.mutable.Buffer[String]()
+    while (tuples.hasNext) {
+      b += tuples.next.getString("line")
+    }
+    b.toSeq
+  }
+
   def main(args:Array[String]) = {
-    val inputPath = args(0)
-    val outputPath = args(1)
-    
-    val sourceScheme = new TextDelimited(new Fields("tokens"), false, ",")
-    val source = new Hfs(sourceScheme, inputPath)
-    val sinkScheme = new TextLine(new Fields("bc"))
-    val sink = new Hfs(sinkScheme, outputPath, SinkMode.REPLACE)
+    if (args.size < 1) throw new IllegalArgumentException()
 
-    var p = new Pipe("bayes")
-    p = new Each(p, new BayesSplitter)
-    p = new GroupBy(p, new Fields("dummy"))
-    p = new Every(p, new BayesAggregator)
-    p = new Each(p, new Fields("bayes"), new Identity)
+    if (args(0) == "train") {
+      assert(args.size == 4)
+      val featNamePath = args(1)
+      val inputPath = args(2)
+      val outputPath = args(3)
+      
+      val featNames = readFile(featNamePath).head.split(",").toList
 
-    val flowConnector = new FlowConnector()
-    val flow = flowConnector.connect("bayes", source, sink, p)
-
-    flow.complete()
+      val source = new Hfs(new TextLine(), inputPath)
+      val sink = new Hfs(new TextLine(), outputPath, SinkMode.REPLACE)
+      var p = new Pipe("bayes")
+      p = new Each(p, new Insert(new Fields("group"), "1"), Fields.ALL)
+      p = new GroupBy(p, new Fields("group"))
+      p = new Every(p, new BayesAggregator(featNames))
+      p = new Each(p, new Fields("bayes"), new Identity)
+      val flowConnector = new FlowConnector()
+      val flow = flowConnector.connect("bayes", source, sink, p)
+      flow.complete()
+    }
+    else if (args(0) == "classify") {
+      assert(args.size == 4)
+      val bcSource = new Hfs(new TextLine(), args(1))
+      val dataSource = new Hfs(new TextDelimited(new Fields("tokens"), false, ","),
+                               args(2))
+      val multi = new MultiSourceTap(bcSource, dataSource)
+      val sink = new Hfs(new TextLine(), args(3), SinkMode.REPLACE)
+      var p = new Pipe("bayes")
+      p = new Each(p, new Debug())
+      val flowConnector = new FlowConnector()
+      val flow = flowConnector.connect("bayes", dataSource, sink, p)
+      val jc = flow.getJobConf()
+    }
+    else {
+      throw new IllegalArgumentException()
+    }
   }
 
 }
